e2d404c4311937e47472b07d529e92945ff0bb15
ShahidAbbasKhan <developershahid297@gmail.com>
2022-08-14 13:55:54 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,714 @@
+// // console.warn("WARNING ")
+// // var a=5;
+// // var b=6;
+// // console.log(a+b);
+// // var a=5;
+// // var b="aaa";
+// // console.log(a+b);
+// // {
+// // let a=5;
+// // console.log(a); }
+// // If statement Checking
+// // var temp;
+// // if(temp==undefined)
+// // console.log("Yes its UNDEFINED");
+// // else
+// // console.log("It is defined")
+// //New Checking Type of Variable
+// //  var abc="Shahid";
+// //  console.log(typeof abc);
+// //  abc =123;
+// //  console.log(typeof abc);
+// //  abc=true;
+// //  console.log(typeof abc);
+// // Multiplication of Variables
+// // var a=5;
+// // x=a*5;
+// // alert(x);
+// // console.log(x);
+// // document.write(x);
+// //Case Sensitive
+// // var ali="I am ali";
+// // console.log(ali);
+// // var Ali="I am Ali";
+// // console.log(Ali);
+// // var ALI="I AM ALI";
+// // console.log(ALI);
+// // //Operators
+// // var a = 15;
+// // var b = 3;
+// // var c = a + b; console.log(c);
+// // var d = a - b; console.log(d);
+// // var e = a * b; console.log(e);
+// // var f = a / b; console.log(f);
+// // var g = a % b; console.log(g);
+// // var h = a ** b; console.log(h);
+// //increment  and Decrement
+// // num=2;
+// // num+=8; //num=num+8;
+// // console.log(num);
+// // var age=22;
+// // ++age; //pre increment +1
+// // console.log(age);
+// // --age; // pre decrement -1
+// // console.log(age);
+// // var Age=12; // For it Store values in another variable first
+// // var abc=Age++;
+// // console.log(Age);
+// // console.log(abc);
+// // var Age=12;
+// // var abc=Age--;
+// // console.log(Age);
+// // console.log(abc);
+// // var age = 12;
+// // var newAge = age--;
+// // console.log(age);
+// // console.log(newAge);
+// //Some Operation
+// // var a=5;
+// // var b=2;
+// // var c=a++ -b;
+// // console.log(a);
+// // console.log(b);
+// // console.log(c);
+// // var a = 4;
+// // var b = 3;
+// // var c = a++ + --b - --a ; //4+2-4 values of a will not chnge for c
+// // // console.log(--a);
+// // console.log(a); // a values will be changed after c operating
+// // console.log(b);
+// // console.log(c);
+// // var a = 4;
+// // var b = 3;
+// // var c = ++a + b++ - a + --b;
+// // var d = ++a + b++ - a + b--;
+// // console.log(a); // 5
+// // console.log(b); // 3
+// // console.log(c);
+// // console.log(d);
+// //Concatenation
+// // var fname="Shahid";
+// // var lname="Khan";
+// // console.log(fname+" "+lname);
+// // console.log(typeof fname,typeof lname);
+// //Prompt Use
+// // var Q="What is Your Name";
+// // var A="Shahid";
+// // E=prompt(Q,A);
+// // console.log("Name:"+" "+E)
+// // var itsname=prompt("your name","Ali");
+// // console.log(itsname);
+// // // var myage=22;
+// // // var age=prompt("Which num do you want add in your Age");
+// // // var updtAge=myage+age;
+// // // console.log(updtAge);
+// // // console.log(typeof updtAge);
+// // //Confirm use 
+// // var message=confirm("Do you Want to Send Message ?");
+// // console.log(message);
+// //Convert String into Integer
+// // var Age=prompt("Enter your Age");
+// // var Att=confirm("Is is your Age");
+// // var add=5;
+// // var total=parseInt(Age)+add;
+// // console.log(Att);
+// // console.log(total);
+// // console.log(typeof total);
+// // let a=prompt("what is your name");
+// // let b=5;
+// // let c=parseInt(a)+b;
+// // console.log(c);
+// // console.log(typeof c);
+// //ParseFloat
+// // var age = prompt("What is your age");
+// // var num = 4;
+// // var sum = parseFloat(age) + num;
+// // console.log(sum);
+// //Number Function
+// // var age = prompt("What is your age");
+// // var num = 4;
+// // var sum = Number(age) + num;
+// // console.log(sum);
+// // console.log(typeof sum);
+// // console.log(Number(true));
+// // console.log(Number(false));
+// //Number to String .....toFixed
+// // var aaa=23.89754;
+// // var bb=aaa.toFixed(6);
+// // console.log(bb);
+// // console.log(typeof bb);
+// // //toPrecision
+// // let num = 25.3852;
+// // let a = num.toPrecision();
+// // let b = num.toPrecision(2);
+// // let c = num.toPrecision(3);
+// // let d = num.toPrecision(10);
+// // console.log(a);
+// // console.log(b);
+// // console.log(c);
+// // console.log(d);
+// //Comparing and Logical Operators
+// // var a=5;
+// // console.log(a==5);
+// // console.log(a==="5");
+// // console.log(a=="5");
+// // var b=3;
+// // if(b==a)
+// // { console.log("Yes A and B are Equal")
+
+// // }
+// // else
+// // {
+// //     console.log("A and B are not Equal");
+// // }
+// // var Name=prompt("Enter your name");
+// // var received="shahid";
+// // if(Name===received)
+// // {
+// //     console.log("Yes name is True")
+// // }
+// //  else 
+// // {
+// //     console.log("You have Enter Wrong Name");
+// // }
+// //Now  Else If
+// // var marks=prompt("Enter Your Marks");
+// // var Age=prompt("Enter Your Age");
+// // if (marks>70)
+// // {
+// //     console.log("You have A grade")
+// // }
+// //  else if(marks>70 && Age<20)
+// //  {
+// //      console.log("Congrats You have A grade  and 100$ Prize")
+// //  }
+// //  else
+// //  {
+// //      console.log("You have B grade")
+// //  }
+// // var marks=123;
+// // var age=19;
+// // if(marks>=120 && marks<200 && age>10)
+// // {
+// //     console.log("You have Good Marks");
+// //      if(age<20)
+// //      {
+// //          console.log("You have Good Age");
+// //      }
+// // }
+// //Number into Boolean
+// // var exe=234;
+// // if(exe)
+// // {
+// //     console.log("You have 234");
+// // }
+// // else{
+// //     console.log("it is not 234");
+// // }
+// //Null to boolean
+// // var a;
+// //null will be converted to false, else will be executed
+// // if(a){
+// // console.log("In If a = "+a);
+// // }
+// // else{
+// // console.log("In else a = "+a);
+// // }
+// //For Loop
+// // var number=prompt("Enter Number");
+// // for(var a=1; a<11; a++)
+// // {
+// //     console.log(number+"x"+a +" = " +(a*number));
+// // }
+// //While Loop
+// // let j = 0;
+// // while (j < 10) {
+// //  console.log("I am Ali");
+// //  j +=1;
+// // }
+// //Use of Break
+
+// // for (var i = 0; i < 8; i++){
+// //     if(i == 4) {
+// //     break;
+// //     }
+// //     console.log("I = "+i);
+// //     }
+// //     //Use of Continue
+// //     for (var i = 0; i < 8; i++){
+// //         if(i == 4) {
+// //         continue;
+// //         }
+// //         console.log("I = "+i);
+// //         }
+// //Nested For Loop
+// // for(var a=1;a<4;a++){
+// //     for(var b=1;b<4;b++)
+// //     console.log(a+" "+b +"="+a*b)
+// // }
+// //Number is Even or Odd
+// // var num=prompt("Enter Number");
+// // var num2=parseInt(num);
+
+// // if(num2 % 2 ==0){
+// //     console.log("Number is Even")
+
+// // }
+// // else
+// // {
+// //     console.log("Number is Odd")
+// // }
+// //Number is Positive or Negative
+// // var num=prompt("Enter Number");
+// // var num2=parseInt(num);
+// // if(num2>=0){
+// //     console.log("Number is Positive")
+// // }
+// // else{
+// //     console.log("Number is Negative")
+// // }
+// //Alphabet is Vowel or not
+// // var alpha1=prompt("Enter Alphabet");
+// // if(alpha1=="a" || alpha1=="A" ||alpha1=="e" || alpha1=="E" || alpha1=="i"|| alpha1=="I" || alpha1=="o"|| alpha1=="O" || alpha1=="u"|| alpha1=="U"){
+// //     console.log("Alphabet is Vowel");
+
+// // }
+// // else{
+// //     console.log("Alphanet is Not Vowel"); }
+// // }
+// //Array
+// // var Name=["ali", "ahsan", "ahmed"];
+// // console.log(Name);
+// // // console.log(typeof Name);
+// // // var fruits=new Array("Apple","Mango","orange");
+// // // console.log(fruits);
+// // // console.log(typeof fruits);
+// // // console.log(fruits[2]); //Accessing Value
+// // // console.log(fruits[3]); 
+// // //Updating an Array
+// // Name[3]="Shahid";
+// //Updating Value
+// // Name[1]="Ali";
+// // console.log(Name);
+// // console.log(Name[4]);
+// // //Length of Array
+// // console.log(Name.length);
+// // var vegetable=["potato","tomato","chilly"]
+// // vegetable.push("123");
+// // console.log(vegetable);
+// // vegetable.push("124", "125", "126");
+// // console.log(vegetable);
+
+// //LIFO(Push Pop Method) Stack
+// // var numb=[1,2,3,4];
+// // numb.push(5,6,7);
+// // a=numb.length;
+// // console.log(a);
+// // b=numb.push(8);
+// // console.log(numb);
+// // console.log(numb.length)
+// // numb.pop();
+// // console.log(numb);
+// // numb.pop();
+// // console.log(numb);
+// //FIFO(Queue) Push and Shift
+// //Push will add at last and Shift will Remove from start
+// // let arr=[1,2,3,4,5];
+// // console.log(arr);
+// // num=arr.shift();
+// // console.log(num);
+// // console.log(arr);
+
+// //Unshift (to add on Start and previous will be fwd)
+// // var foods = [];
+// // foods.push("Pizza");
+// // foods.push("Burger");
+// // foods.push("Snacks");
+// // console.log("Length "+foods.length); 
+// // console.log(foods[0]);
+// // foods.unshift("Sandwich");
+// // console.log(foods[0]); 
+// // console.log(foods);
+// //Accessing All elements in Array
+// // var names=["ali", "ahsan","skinder","mehbob"]
+// // for(i=0;i<names.length ;i++)
+// // {
+// //     console.log(names[i]);
+// // }
+// //
+// // var arr1=[1,2,3,4,5];
+// // var num=prompt("Enter number");
+// // arr1.push(num);
+// // for(i=0;i<arr1.length;i++){
+// //     if(typeof arr1[i]==="string")
+// //     {
+// //         console.log("This is numb by User"+" "+arr1[i]);
+// //     }
+// // }
+// // console.log(arr1);
+// //Splice Function..start,howmany,add,add
+// // start positive r to l... start negative l to  r
+// // var foods = ["Pizza", "Burger", "Snacks", "Biryani", "Chicken"];
+// // // var b = foods.splice(-2,1,"aaas");
+// // console.log(b);
+// // console.log(foods);
+// //String Fuction ... toLowerCase and toUpperCase
+// // var yourname="Shahid";
+// // console.log(yourname.toLowerCase());
+// // console.log(yourname.toUpperCase());
+// // ///**Splice original array is affected but not in Slice */
+// // //Slice character set
+// // var b=yourname.slice(3,7);
+// // console.log(b);
+// // var c=yourname.slice(1); // will show complete by missing 2nd paramtr
+// // console.log(c);
+// // var d=yourname.slice(-5,-2);
+// // console.log(d);
+// //IndexOf 
+// // let e = yourname.indexOf("i");
+// // console.log(e);
+// // let f = yourname.indexOf("h",2); //  show fwd 
+// // console.log(f);
+// // let str =prompt("Enter Your Name");
+// // let substr = 'S';
+// // let count = 0;
+// // let index = str.indexOf(substr);
+// // while(index !== -1) {
+// //  count++;
+// //  index = str.indexOf(substr, index + 1);
+// // }
+// // console.log(count);
+//  //lastIndexOf .. Reverse value find right to left in string
+// //  var x="shdahi";
+// //  var v=x.lastIndexOf("d");
+// //  console.log(v);
+// //  var y=x.lastIndexOf("d",3);
+// //  console.log(y);
+// // let text = "Hello planet earth, you are a great planet.";
+// // let result = text.lastIndexOf("planet");
+// // console.log(result);
+// // let result1 = text.lastIndexOf("planet",30);
+// // console.log(result1);
+// //charAt
+// // let Name="Shahid Khan Niazi";
+// // var ch=Name.charAt(12);
+// // console.log(ch);
+// //replace function
+// // var Name="My name is Shahid My class is NFT";
+// // var rep=Name.replace("My","Your");
+// // console.log(rep);
+// // var rep1=Name.replace(/My/gi,"Your");
+// // console.log(rep1);
+// //split Function
+// // const Name="shahid,abbas,khan";
+// // console.log(Name.split(","));
+// // //Math.round function on Number
+// // //ceil goes up to given floor goes down to given
+// // var n=123.2;
+// // var m=23.4;
+// // // console.log("round on n",Math.round(n));
+// // // console.log("round on m",Math.round(m));
+// // // console.log("ceil on n",Math.ceil(n));
+// // // console.log("ceil on n",Math.ceil(m));
+// // // console.log("round on m",Math.round(m));
+// // // console.log("floor on m",Math.floor(m));
+// // // console.log("floor on n",Math.floor(n));
+// // let l=Math.random(n);
+// // console.log(l);
+// // var x=(l*5)+2;
+// // console.log("random on x",Math.ceil(x));
+// // var a=123.6797;
+// // var b=a.toFixed(2);
+// // console.log(b);
+// // console.log(typeof b);
+// // console.log(a.toPrecision(4));
+// //Date Function with get and set
+// // var todayDate= new Date();
+// // console.log(todayDate);
+// // var b=new Date();
+// // console.log(b);
+// // // console.log(b.getDay());
+// // // console.log(b.getTime());
+// // // console.log(b.getFullYear());
+// // // console.log(b.getHours());
+// // // console.log(b.getDate());
+// // // console.log(b.getSeconds());
+// // // console.log(b.getMilliseconds());
+// // // var c=new Date(0); // 1970 UTC
+// // // console.log(c);
+// // b.setFullYear(2009);
+// // b.setMinutes(13);
+// // console.log(b);
+// //Checking Name of The Day
+// // var daysList = ["Sun", "Mon", "Tue", "Wed",
+// // "Thu", "Fri", "Sat"];
+// // var date = new Date("June 14 2019 10:45:25");
+// // var day = date.getDay(); // 5
+// // var nameOfDay = daysList[day]; 
+// // console.log(nameOfDay);
+// //Functions
+// // function func1()
+// // {
+// //     let a=12;
+// //     let b=45;
+// //     let c=a+b;
+// //     return c;
+
+// // }
+// // let cal=func1();
+// // console.log(cal);
+// // function hello()
+// // {
+// //    console.log("Hello");
+// // }
+// // hello();
+// // function sum(a,b){ 
+// //     var num=a*b;
+// //     console.log(num);
+// // }
+// // sum(4,5);
+
+// // function showMessage(message){
+// //     console.log(message);}
+// //     showMessage("Hello NFT Classmates");
+// // function Grade(Marks){
+// //     if(Marks>90 && Marks<100){
+// //         console.log("You have A Grade");
+// //     }
+// //     else if(Marks>80 && Marks<90){
+// //         console.log("You have B Grade");
+// //     }
+// //     else if(Marks>70 && Marks<80){
+// //         console.log("You have C Grade");
+// //     }
+// //     else{
+// //         console.log("You Are Fail");
+// //     }
+// // }
+// // Grade(56);
+// // Grade(96);
+// // Grade(76);
+// // function Name(yourname){
+// //     console.log(yourname);
+// //     console.log("Hello"+" "+yourname);
+// //     return yourname;// not return string
+// // }
+// // Name("Shahid");
+// // // Name(56);
+// // function multiply(n1,n2){
+// //     return n1*n2;
+// //     // console.log(n1*n2);
+// // }
+// // let x=multiply(12,3);
+// // let b=multiply(2,3);
+// // console.log(x);
+// // console.log(b);
+// // console.log(multiply(11,2));
+// // function multiply(num1, num2){
+// //     return num2; // function execution ends here
+// //     return num1 * num2;
+// //     }
+// //     var a = multiply(3,6);
+// //     console.log(a);
+    
+// // function addi(n,m){
+// //     // return n+m+7;
+// //     console.log(n+m+7);
+// // }
+// // addi(2,3);
+// // function addi1(m,n){
+// //     return m+n;
+    
+// // }
+// // let x=addi1(2,3)+7;
+// // console.log(x);
+// // function func1(n,m){
+// //     return n+m;
+// // }
+// // function func2(a,b){
+// //     return func1(a,b)+a;
+// // }
+// // // let x=func1(2,3);
+// // // console.log(x);
+// // let y=func2(3,4);
+// // console.log(y);
+// // function multiply(num1, num2){
+// //     return num1 * num2;
+// //     }
+// //     function sum(a, b){
+// //     return a + b;
+// //     }
+// //     // Call multiply first and result passed to sum
+// //     var total = sum(multiply(3,4), 2) + 6;
+// //     console.log(total);
+// //Global and Local Variable
+// // var w=2;
+// // function f2(){
+// //     let x=4;
+// //     console.log(w+x);
+// // }
+// // f2(); //6
+// // console.log("Here is Global w=",+w); //2
+// // var v=2;
+// // function f3(){
+// //     let x=4;
+// //     v=x+6;
+// //     console.log(v); //10
+// // }
+// // f3();
+// // console.log("Here is Global v=",+v); //10
+// // var z=2;
+// // function f4(){
+// //     var z=5; //if local z is not iniated then global z value will be used
+// //     var k=z+5;
+// //     console.log(k);
+// // }
+// // f4();
+// // console.log(z);
+// // a = 7; // Without var still Global Variable
+// // function sum(){
+// // b = 6; // Global variable because its without
+// // var a = b + 5;
+// // console.log("A "+a);// Accessing global variable
+// // }
+// // sum();
+// // console.log("B "+b);
+// //Function can be defined as it using an Expression
+// // var sum = function (a, b){
+// //     return a + b;
+// //     };                ///; will be used in it
+// //     var c = sum(4,5);
+// //     console.log(c);
+// //     var square = function(num) {
+// //         return num * num; };
+// //         var b = square(4);
+// //         console.log(b);
+// // var num = 5;
+// // function changeValue(a){
+// // // a = 7;
+// // c=7+a;
+// // return num+3; // change to a will not affect num
+// // }
+// // console.log(changeValue(num));
+// // console.log(c);
+
+// //Arrays and Object passing as Referenvce in Function
+// // var arr = [4, 6, 7, 9];
+// // function updateArray(val){ // array received in val
+// // val[1] = 57; // updating val will also update arr
+// // // 6 before calling function
+// // }
+// // console.log(arr[1]);
+// // updateArray(arr);
+// // console.log(arr[1]);
+
+// // var arr1=[1,2,3,4,5]; /// by array
+// // function fn(num){
+// //     num[1]=4;
+// // }
+// // console.log(arr1);
+// // fn(arr1);
+// // console.log(arr1);
+// // var obj1={name:"shahid",age:23,class:"bs"}; // by object
+// // function fn(val){
+// //     val.name="Ali";
+// // }
+// // console.log(obj1);
+// // console.log(obj1.name)
+// // fn(obj1);
+// // console.log(obj1);
+// // console.log(obj1.name);
+// //Recursive Function
+
+// // program to count down numbers to 1
+// // function countDown(number) {
+
+// //     // display the number
+// //     console.log(number);
+
+// //     // decrease the number value
+// //     const newNumber = number - 1;
+
+// //     // base case
+// //     if (newNumber > 0) {
+// //         countDown(newNumber);
+// //     }
+// // }
+
+// // countDown(4);
+
+// // // program to find the factorial of a number
+// // function factorial(x) {
+
+// //     // if number is 0
+// //     if (x === 0) {
+// //         return 1;
+// //     }
+
+// //     // if number is positive
+// //     else {
+// //         return x * factorial(x - 1);
+// //     }
+// // }
+
+// // const num = 3;
+
+// // // calling factorial() if num is non-negative
+// // if (num > 0) {
+// //     let result = factorial(num);
+// //     console.log(`The factorial of ${num} is ${result}`);
+// // }
+// //Switch Statement
+// // var day=5;
+// // switch(day) {
+// //     case 1:
+// //         console.log("Today is Monday");
+// //         break;
+// //     case 2:
+// //         console.log("Today is Tuesday");
+// //         break;
+// //     case 3:
+// //         console.log("Today is Wednesday");
+// //         break;
+// //     default:
+// //         console.log("Today is Sunday")
+// // }
+// //Grouping Case in Switch 
+// // var day = 0;
+// // switch (day) {
+// // case 6:
+// // case 0:
+// // case 5:
+// // console.log("Yaaaa! It's Weekend");
+// // break;
+// // default:
+// // console.log("Looking forward to the Weekend");
+// // }
+// // var day = 6; switch (day)
+// // {
+// // case 6: // No break console.log("Today is Saturday");
+// // case 0: // No break in last case, both will execute
+// // console.log("Today is Sunday");
+// // break; default:
+// // console.log("Looking forward to the Weekend");
+// // }
+
+// function Prime(number){
+//     if(number<2){
+//         console.log("Number is Not Prime Less than 2");}
+//     for(var i=2;i<number;i++){
+//         if(number%i===0)
+//         return "Number is not Prime";
+//     }
+    
+//     return"Number is Prime";
+// }
+// console.log(Prime(7));
+
+
+// // for(var i=1;i<8;i++){ //Triangle shape
+// //     for(var j=1;j<=i;j++){
+// //        document.write("*&nbsp");
+// //     }
+// //     document.write("<br>");
+// // }
